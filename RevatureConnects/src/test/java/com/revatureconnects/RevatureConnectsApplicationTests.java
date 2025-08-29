2c9ba2fcad679a9b75076057bfde5442c85e7a12
YoganandaPuchakayala <YoganandaPuchakayala@Rev-PG02C4T0>
2025-08-30 00:38:54 +0530

    Project Structure.

---
+++ RevatureConnects/src/test/java/com/revatureconnects/RevatureConnectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revatureconnects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevatureConnectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
